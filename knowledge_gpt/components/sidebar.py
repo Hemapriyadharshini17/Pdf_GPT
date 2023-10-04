0638981127169bfe7d73f7b45e3c794c6d63a1b3
Hemapriya17 <hema.learny@gmail.com>
2023-10-04 11:16:32 +0530

    first commit

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-from knowledge_gpt.components.faq import faq
 from dotenv import load_dotenv
 import os
 
@@ -9,12 +8,6 @@ load_dotenv()
 
 def sidebar():
     with st.sidebar:
-        st.markdown(
-            "## How to use\n"
-            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
-            "2. Upload a pdf, docx, or txt file📄\n"
-            "3. Ask a question about the document💬\n"
-        )
         api_key_input = st.text_input(
             "OpenAI API Key",
             type="password",
@@ -25,19 +18,3 @@ def sidebar():
         )
 
         st.session_state["OPENAI_API_KEY"] = api_key_input
-
-        st.markdown("---")
-        st.markdown("# About")
-        st.markdown(
-            "📖KnowledgeGPT allows you to ask questions about your "
-            "documents and get accurate answers with instant citations. "
-        )
-        st.markdown(
-            "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
-            "with your feedback and suggestions💡"
-        )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
-        st.markdown("---")
-
-        faq()
