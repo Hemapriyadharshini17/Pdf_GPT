2cc60fe44ba25721ee0640250e2c66a8b211a78a
Hemapriyadharshini <107750354+Hemapriyadharshini17@users.noreply.github.com>
2023-10-06 16:24:07 +0530

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -26,7 +26,7 @@ MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
 
-st.set_page_config(page_title="TestrunzGPT", page_icon="📖", layout="wide")
+st.set_page_config(page_title="PdfGPT", page_icon="📖", layout="wide")
 st.header("📖PdfGPT")
 
 # Enable caching for expensive functions
